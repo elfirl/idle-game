7951a0b24a2a50e8e1b6221af6d711642a26faae
elfirl <mikebilter@Got-Mike-2.local>
2016-01-07 20:36:39 -0500

    All of the core logic is working.

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,20 +1,36 @@
-var numberOfClicks = 0;
+var gameData = {
+	numberOfClicks: 0,
+	multiplier: 1,
+	autoClicks: 0,
+};
 
-$( document ).ready(function() {
+$(document).ready(function() {
 
 	// Display initial clicks on page load.
-	$(".total-clicks").text(numberOfClicks);
+	$(".total-clicks").text(gameData.numberOfClicks);
 
     // Update the number of clicks, and show it on the page.
-    $(".rectangle").on("click" , function() {
-    	numberOfClicks = numberOfClicks + 1;
-    	$(".total-clicks").text(numberOfClicks);
+    $(".click-button").on("click", function(event) {
+    	gameData.numberOfClicks = gameData.numberOfClicks + gameData.multiplier;
+    	$(".total-clicks").text(gameData.numberOfClicks);
     });
 
+    // Upgrades the number of clicks you get when you "Click me!".
+    $(".upgrade-button").on("click", function(event) {
+    	gameData.multiplier++;
+    });
 
+    // Increases autoClicks when the button is pressed.
+    $(".auto-clicks-button").on("click", function(event) {
+    	gameData.autoClicks++;
+    	console.log(gameData.autoClicks);
+    })
 
-
-
+	function totalAll() {
+		gameData.numberOfClicks = gameData.numberOfClicks + gameData.autoClicks;
+		$(".total-clicks").text(gameData.numberOfClicks);
+	}
+	setInterval(totalAll, 1000);    
 
 
 
